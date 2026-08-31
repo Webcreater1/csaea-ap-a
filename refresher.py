ae7d5cd16a09e8c2c05f6cee701d5e59e7dfcadf
Webcreater1 <78916995+Webcreater1@users.noreply.github.com>
2026-08-31 14:24:07 +0000

    init

---
+++ refresher.py
@@ -0,0 +1,11 @@
+animals = ['cat', 'dog', 'blobfish', 'shark', 'owl', 'toad']
+def check_animal():
+    for i in animals:
+        if i in animals == "": animals = ['cat', 'dog', 'blobfish', 'shark', 'owl', 'toad']
+            quantity += 1
+    if quantity == 1:
+        print("animal is in the list")
+    else:
+        print('animal is not in the list')
+    check_animal('cat')
+    check_animal('penguin')
