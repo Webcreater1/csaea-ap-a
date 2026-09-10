636595826edbd6c43f941dfbae12935ea2c48fe4
Webcreater1 <78916995+Webcreater1@users.noreply.github.com>
2026-09-10 13:08:31 +0000

    adds basic class skeleton

---
+++ BasicClass.java
@@ -0,0 +1,24 @@
+public class BasicClass {
+    public static void main(String[] args) {
+        
+        //atributes and date
+        //variables
+
+
+
+        //constructor --- special method called every time you make an object
+        //same name as the class followed by parenthesis
+        // BasicClass()
+
+
+        //behaviors / methods
+        //you can write as many as you want
+
+
+
+
+
+
+    }
+    
+}
