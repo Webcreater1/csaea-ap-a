3918c60865dac0290db397e37582477bd08333bd
Webcreater1 <78916995+Webcreater1@users.noreply.github.com>
2026-09-24 14:50:09 +0000

    class and tester class practice

---
+++ CrashCourse/Car.java
@@ -0,0 +1,85 @@
+package CrashCourse;
+
+    public class Car {
+    
+        private int milesPerGallon;
+        public double cost;
+        private boolean isUsed;
+        private boolean isDamaged;
+        private int milesDriven;
+        private String brand;
+        public String licencePlate;
+
+        Car(double cost, String brand, String licencePlate) {
+            this.cost = cost;
+            this.brand = brand;
+            this.licencePlate = licencePlate;
+
+            milesPerGallon = 26;
+            isUsed = true;
+            isDamaged = false;
+            milesDriven = 87000;
+
+        }
+
+        public void crash(int damageAmount) {
+            if (damageAmount > 10) {
+                System.out.println("Sorry man your car is wrecked!");
+                isDamaged = true;
+            }
+            else if (damageAmount > 5 && damageAmount < 10) {
+                cost -= 25000; 
+                System.out.println("You've got some major damage but its fixable!"); 
+            }
+            else if (damageAmount < 5) {
+                cost -= 10000;
+                System.out.println("You've got some minor damage but its easy to fix!");
+            }
+        }
+    
+        public void changeLicensePlate (String newLicensePlateNumber) {
+            licencePlate = newLicensePlateNumber;
+            System.out.println("Your new licence plate number is " + newLicensePlateNumber);
+        }
+
+        public void drive (int driveMiles) {
+            milesDriven += driveMiles;
+            isUsed = true;
+        }
+
+        public void maintenance (int degreeDone) {
+            if (degreeDone <= 1) {
+                cost += 2500;
+                milesPerGallon += 1;
+                System.out.println("You've improved your car!");
+            }
+            if (degreeDone < 3 && degreeDone > 1) {
+                cost += 5000;
+                milesPerGallon += 2;
+                System.out.println("You've improved your car by a good amount!");
+            }
+            if (degreeDone >= 3) {
+                cost += 7500;
+                milesPerGallon += 3;
+                System.out.println("You've improved your car by a lot!");
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
---
+++ CrashCourse/CarTester.java
@@ -0,0 +1,27 @@
+package CrashCourse;
+
+    public class CarTester {
+        
+        public static void main(String[] args) {
+
+            Car c1 = new Car(32000, "Toyota", "LK16PN");
+            Car c2 = new Car(48000, "Volkswagen", "PO975N");
+
+            c1.cost;
+            c1.crash(6);
+            c1.cost;
+            c1.changeLicensePlate("RVW65L");
+            c1.drive(147);
+            c1.maintenance(2);
+
+            c2.crash(13);
+            c2.licencePlate
+            c2.changeLicensePlate("M0N3YZ");
+            c2.licencePlate
+            c2.drive(273);
+            c2.maintenance(3);
+
+
+
+        }
+    }
